116d3c91aab1b79306dc73332229b160e868f23c
Lorena Mesa <me@lorenamesa.com>
2021-04-03 12:01:38 -0500

    Create hello_world.py

---
+++ hello_world.py
@@ -0,0 +1,2 @@
+print("Did you know that NASA spacesuits cost ~ $12 million USD? That's much better than their original cost of ~ $22 million (in the 1970s)! Wild! Space is awesome!")
+#TODO: Add your fact here!

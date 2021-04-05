af48f0a1a06b9d87c0ffabd071f78dffc339d803
lorenanicole <me@lorenamesa.com>
2021-04-05 17:00:14 -0500

    Add tests; modify README

---
+++ tests/test_hello_world.py
@@ -0,0 +1,18 @@
+from mock import patch
+
+from hello_world import MSGS
+
+
+@patch('hello_world.print_msg')
+def test_failing_test(mock_call_me):
+    for msg in MSGS:
+        mock_call_me(msg)
+    # TODO: Fixme so I work!
+    assert mock_call_me.call_count == 1
+
+
+@patch('hello_world.print_msg')
+def test_print_all_msgs(mock_call_me):
+    for msg in MSGS:
+        mock_call_me(msg)
+    assert mock_call_me.call_count == len(MSGS)
